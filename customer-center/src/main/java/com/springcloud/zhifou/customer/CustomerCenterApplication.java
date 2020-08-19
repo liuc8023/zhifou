e87cb014d092c39fbee92f889485342fbc55ae63
liuc8023 <13764402356@163.com>
2020-08-19 17:05:59 +0800

    整合SpringCloud For Alibaba 与Nacos

--- customer-center/src/main/java/com/springcloud/zhifou/customer/CustomerCenterApplication.java
+++ customer-center/src/main/java/com/springcloud/zhifou/customer/CustomerCenterApplication.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
 @Log4j2
 public class CustomerCenterApplication {
     public static void main(String[] args) {
